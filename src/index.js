2a3bd1335ba7ea574f3e9497f3028c76901d8ccb
alex <arodriguez202020@gmail.com>
2021-12-08 17:06:13 -0600

    import cars data from practice.js, destructure cars array and assign variable to top speed and colour for each car brand

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import animals, { useAnimals } from "./data";
+// import animals, { useAnimals } from "./data";
 
 // //Destructuring Arrays
 // // console.log(animals);
-const [cat, dog] = animals;
+// const [cat, dog] = animals;
 // // console.log(cat);
 
-function AnimalSound() {
-  const [animal, makeSound] = useAnimals(cat);
-  console.log(animal);
-  makeSound();
-}
+// function AnimalSound() {
+//   const [animal, makeSound] = useAnimals(cat);
+//   console.log(animal);
+//   makeSound();
+// }
 
-AnimalSound();
+// AnimalSound();
 
 // //Destructuring Objects
 // // const { name, sound} = cat;
@@ -21,43 +21,43 @@ AnimalSound();
 // // console.log(food);
 
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import cars from "./practice";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// const [honda, tesla] = cars;
+const [honda, tesla] = cars;
 
-// const {
-//   speedStats: { topSpeed: hondaTopSpeed }
-// } = honda;
-// const {
-//   speedStats: { topSpeed: teslaTopSpeed }
-// } = tesla;
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
 
-// const {
-//   coloursByPopularity: [hondaTopColour]
-// } = honda;
-// const {
-//   coloursByPopularity: [teslaTopColour]
-// } = tesla;
+const {
+  coloursByPopularity: [hondaTopColour],
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour],
+} = tesla;
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//       <th>Top Colour</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
