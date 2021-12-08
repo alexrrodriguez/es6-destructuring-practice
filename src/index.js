34522d611353b3385fd9f19d8422f6498a1b4ac2
alex <arodriguez202020@gmail.com>
2021-12-08 16:54:53 -0600

    destructure animals array for example in index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import animals, { useAnimals } from "./data";
 
-ReactDOM.render(
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
-  </table>,
-  document.getElementById("root")
-);
+// //Destructuring Arrays
+// // console.log(animals);
+const [cat, dog] = animals;
+// // console.log(cat);
+
+function AnimalSound() {
+  const [animal, makeSound] = useAnimals(cat);
+  console.log(animal);
+  makeSound();
+}
+
+AnimalSound();
+
+// //Destructuring Objects
+// // const { name, sound} = cat;
+// // const { name: catName, sound: catSound } = cat;
+// // const { name = "Fluffy", sound = "Purr" } = cat;
+// // const {feedingRequirements: {food, water} } = cat;
+// // console.log(food);
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import cars from "./practice";
+
+// const [honda, tesla] = cars;
+
+// const {
+//   speedStats: { topSpeed: hondaTopSpeed }
+// } = honda;
+// const {
+//   speedStats: { topSpeed: teslaTopSpeed }
+// } = tesla;
+
+// const {
+//   coloursByPopularity: [hondaTopColour]
+// } = honda;
+// const {
+//   coloursByPopularity: [teslaTopColour]
+// } = tesla;
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//       <th>Top Colour</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
